79fd4e5a02b03406c6de27bf844c428b70a25598
marcusp619 <marcusp619@gmail.com>
2019-01-08 15:49:43 -0700

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const nasaImages = require('./images');
+const cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.set('port', process.env.PORT || 3001);
 
